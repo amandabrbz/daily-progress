c89b5f4a551e19d89ed1d683a68f32a21238b5c0
amandabrbz <amanda.brbz@hotmail.com>
2021-03-13 15:55:42 -0300

    ajusta sombreamento do title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,16 +1,14 @@
 import styled from 'styled-components'
 
-
 function Title(props) {
   const { text } = props
   const Title = styled.h1`
     font-size: 2rem;
     font-family: sans-serif;
     color: #b19cd9;
+    text-shadow: 1px 1px #fff;
   `
-  return (
-    <Title>{text}</Title>
-  )
+  return <Title>{text}</Title>
 }
 
 export default Title
