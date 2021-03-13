148e3da3c62611905a96635e394f4a0ae28a0415
amandabrbz <amanda.brbz@hotmail.com>
2021-03-13 16:20:01 -0300

    adiciona um mock de um array q contem dias da semana e porcentagem e passa para o tracker

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,22 +1,59 @@
 import styled from 'styled-components'
+import Tracker from './Tracker'
 
 const Board = (props) => {
   const Board = styled.section`
     margin: 20px auto;
     display: flex;
-    flex-direction: center;
+    flex-direction: column;
     align-items: center;
     min-width: 20vw;
-    min-height: 20vh;
     max-width: 40vw;
     max-height: 70vh;
     background: #fff;
     border-radius: 20px;
-    box-shadow: 2px 5px 20px 0.4px rgba(0,0,0,0.1);
+    box-shadow: 2px 5px 20px 0.4px rgba(0, 0, 0, 0.1);
     padding: 16px;
   `
 
-  return <Board>hello</Board>
+  const days = [
+    {
+      day: 'S',
+      per: 40,
+    },
+    {
+      day: 'M',
+      per: 30,
+    },
+    {
+      day: 'T',
+      per: 60,
+    },
+    {
+      day: 'W',
+      per: 60,
+    },
+    {
+      day: 'T',
+      per: 60,
+    },
+    {
+      day: 'F',
+      per: 90,
+    },
+    {
+      day: 'S',
+      per: 10,
+    },
+  ]
+
+  return (
+    <Board>
+      {days.map((day) => (
+        <Tracker day={day.day} percentage={day.per} key={day.day} />
+      ))}
+    </Board>
+  )
 }
 
 export default Board
